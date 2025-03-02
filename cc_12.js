e82912ddb8b0671fb1a7f9f798d8dcc9842f958f
Vanessa Do <xex.vanessa@gmail.com>
2025-03-02 16:15:55 -0500

    Task 1- created revenue metric card

---
+++ cc_12.js
@@ -0,0 +1,10 @@
+// Task 1 Created Revenue Metric Card 
+
+const dashboard_1 = document.getElementById("dashboard"); // using getElementById
+const dashboard_2 = document.querySelector("#dashboard"); // using querySelector
+const revenueCard = document.createElement("div"); // create a new <div> element 
+// assigned class and id to the new element 
+revenueCard.setAttribute("class", "metric-card"); // setting class attribute 
+revenueCard.setAttribute("id", "revenueCard"); // setting id attribute 
+revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>" // creating card title and placeholder value 
+dashboard_1.appendChild(revenueCard); // new metric card to dashboard container
