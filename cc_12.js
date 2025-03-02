55ecc0e7d64ab077df5320c3c3a6fa4ef8e2d1ae
Vanessa Do <xex.vanessa@gmail.com>
2025-03-02 16:36:16 -0500

    Task 3- implemented dynamic inventory list

--- cc_12.js
+++ cc_12.js
@@ -16,3 +16,19 @@ const metricCards = document.querySelectorAll(".metric-card");
 Array.from(metricCards).forEach(card =>{card.innerHTML += " - Updated";
     card.style.backgroundColor = 'lightgreen';
 });
+
+// Task 3 Dynamic Inventory Management 
+// append new product item to inventory list 
+const inventoryList = document.getElementById('inventoryList');
+// created a function to represent a product 
+function addInventoryItem(productName) {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class','product-item'); // set class attribute 
+    listItem.setAttribute('data-product', productName); // set custom data attribute 
+    listItem.textContent = productName; // setting inner text to product name 
+
+    listItem.addEventListener('click', () =>{
+        inventoryList.removeChild(listItem); // removed clicked items from inventory list 
+    });
+    inventoryList.appendChild(listItem);
+}
